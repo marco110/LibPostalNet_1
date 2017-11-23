c69a0b4dbfd72877de0599cef35567bf47f58edc
AeroXuk <AeroXuk@users.noreply.github.com>
2017-11-23 08:31:47 +0000

    Project is now CLS Compliant, so switching on flag.

--- LibPostalNet/Properties/AssemblyInfo.cs
+++ LibPostalNet/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -18,6 +18,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7154a21b-b8a8-4a1f-a1db-078c1f1b600e")]
